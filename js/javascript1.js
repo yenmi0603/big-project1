279a92aa90de29af666eb44344f172a151ca42ff
yenmi0603 <2251052064mi@ou.edu.vn>
2023-08-22 10:41:24 +0700

    init

--- js/javascript1.js
+++ js/javascript1.js
@@ -13,3 +13,19 @@
     window.onload = () =>{
        Goout();
     };
+   function upload(){
+    fetch("js/student.json").then(res=>res.json()).then(data=>{
+        for(let c of data)
+        {
+            const user = {
+                username: c.username,
+                email: c.email,
+                password: c.password,
+              };
+              let json = JSON.stringify(user);
+              localStorage.setItem(username.value, json);
+        }
+       
+   })
+};
+window.onload=upload();
