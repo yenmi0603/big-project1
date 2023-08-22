130c6c3a6ca916e3e59b7d2d649bd6c5ae179dce
yenmi0603 <139731847+yenmi0603@users.noreply.github.com>
2023-08-22 13:14:59 +0700

    and

--- js/javascript2.js
+++ js/javascript2.js
@@ -1,12 +1,14 @@
 function profile(){
     var user=localStorage.getItem("quanly");
       var user=JSON.parse(user);
+      let e =document.getElementById("avarta-main");
         fetch("js/student.json").then(res=>res.json()).then(data=>{
             let h="";
             for(let c of data)
             {
                 if(c.username=== user.username)
                 {
+                    
                     h=`
                     <h3 class="avatar-text">${c.username}</h3>
                         <div class="account">
@@ -15,15 +17,28 @@ function profile(){
                             <p><i class="fa-regular fa-address-card"></i>${c.khoa}</p>
                         </div>
                     `
+                    e.innerHTML=h;
                 }
             }
-            let e =document.getElementById("avarta-main");
-            if(e!==null)
-               e.innerHTML=h;
+        
+            if(user.username==="")
+            {
+                
+                
+                h=`
+                    <h3 class="avatar-text">Không có dữ liệu</h3>
+                        <div class="account">
+                            <p><i class="far fa-envelope"></i>Không có dữ liệu</p>
+                            <p><i class="fa-regular fa-pen-to-square"></i>Không có dữ liệu</p>
+                            <p><i class="fa-regular fa-address-card"></i>Không có dữ liệu</p>
+                        </div>
+                    `
+                    e.innerHTML=h;
+            }
+               
         })
     
     };
-    window.onload = () =>{
-        profile();
+    window.onload = profile();
     
-    };
+    
